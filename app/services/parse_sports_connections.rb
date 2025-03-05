c65ab8f299e80eade05ffb34326285686968019d
Harrison Hassig <harrisonhassig@gmail.com>
2025-03-05 06:50:43 +0100

    Fix sports_connections parsing

--- app/services/parse_sports_connections.rb
+++ app/services/parse_sports_connections.rb
@@ -8,8 +8,9 @@ class ParseSportsConnections
   def parse
     temp = @result.original.split("\n")
     @result.edition = temp[1].match(/#(\d+)/)[1]
-    @result.score = temp[2..]
-    @result.numeric_score = calculate_numeric_score(temp[2..])
+    score = temp[2..].map { |str| str.gsub(" ", "") }
+    @result.score = score
+    @result.numeric_score = calculate_numeric_score(score)
     @result.timer = @result.numeric_score.to_i
     @result.won = @result.timer.to_i < 8
     @result.gameday_id = Gameday.find_or_create_by!(date: (Date.new(2024, 9, 23) + @result.edition).strftime("%Y-%m-%d")).id
@@ -19,7 +20,9 @@ class ParseSportsConnections
   private
 
   def calculate_numeric_score(score)
+    puts score.last
     score = score[1..] if score.first.include?("Time:")
     score.last.chars.uniq.length <= 1 ? score.size.to_s : "9"
+    score.last
   end
 end
