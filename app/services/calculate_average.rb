3aa2f553aaf4a0a44afb6c8681e716ceabca290c
Harrison Hassig <harrisonhassig@gmail.com>
2025-03-16 05:41:45 -0700

    Averages

--- app/services/calculate_average.rb
+++ app/services/calculate_average.rb
@@ -7,7 +7,7 @@ class CalculateAverage
   end
 
   def average
-    result_stats = Result.where(game: @game, user: @user).pick("SUM(timer) AS total_time, COUNT(*) AS result_count")
+    result_stats = Result.where(game: @game, user: @user).pick(Arel.sql("SUM(timer) AS total_time, COUNT(*) AS result_count"))
     return Average.find_or_create_by!(user: @user, game: @game, score: 0.0) if result_stats.nil? || result_stats[1] == 0
     total_time, result_count = result_stats
     new_score = total_time.to_f / result_count
