d7525c1d25f4462764d922bd753315c20743228e
Harrison Hassig <harrisonhassig@gmail.com>
2025-03-18 16:30:04 -0700

    Leaderboards

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,8 +1,10 @@
 class LeaderboardsController < ApplicationController
   def index
     @leaderboards = LeaderboardsPresenter.new(params[:date].present? ? params[:date] : Date.today.strftime("%Y-%m-%d")).set
+    @gameday = params[:date].present? ? Gameday.find_or_create_by!(date: params[:date]) : Gameday.find_or_create_by!(date: Date.today.strftime("%Y-%m-%d"))
   end
 
   def show
+    @gameday = params[:date].present? ? Gameday.find_or_create_by!(date: params[:date]) : Gameday.find_or_create_by!(date: Date.today.strftime("%Y-%m-%d"))
   end
 end
