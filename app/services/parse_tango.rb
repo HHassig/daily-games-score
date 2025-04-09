adc5604cadda4e1de1efb321b012eaf87447311f
Harrison Hassig <harrisonhassig@gmail.com>
2025-04-08 21:40:48 -0700

    error catching

--- app/services/parse_tango.rb
+++ app/services/parse_tango.rb
@@ -6,22 +6,30 @@ class ParseTango
   end
 
   def parse
-    temp = @result.original.split("\n")
-    @result.edition = temp[0].split("#")[1].split("|")[0].strip
-    @result.score = temp[1..temp.size-2]
-    @result.numeric_score = temp[0].split("|").last.strip
-    @result.timer = CalculateSeconds.new(@result.numeric_score.split(" ").first).convert
-    @result.gameday_id = Gameday.find_or_create_by!(date: (Date.new(2024, 10, 7) + @result.edition).strftime("%Y-%m-%d")).id
+    begin
+      temp = @result.original.split("\n")
+      @result.edition = temp[0].split("#")[1].split("|")[0].strip
+      @result.score = temp[1..temp.size-2]
+      @result.numeric_score = temp[0].split("|").last.strip
+      @result.timer = CalculateSeconds.new(@result.numeric_score.split(" ").first).convert
+      @result.gameday_id = Gameday.find_or_create_by!(date: (Date.new(2024, 10, 7) + @result.edition).strftime("%Y-%m-%d")).id
+    rescue
+      # Silently catch any errors
+    end
     @result
   end
 
   def mobile
-    temp = @result.original.split("\n")
-    @result.edition = temp[0].split("#")[1].split("|")[0].strip
-    @result.score = temp[1..temp.size-2]
-    @result.numeric_score = JSON.parse(@result.score).first.split(" ").first
-    @result.timer = CalculateSeconds.new(@result.numeric_score.split(" ").first).convert
-    @result.gameday_id = Gameday.find_or_create_by!(date: (Date.new(2024, 10, 7) + @result.edition).strftime("%Y-%m-%d")).id
+    begin
+      temp = @result.original.split("\n")
+      @result.edition = temp[0].split("#")[1].split("|")[0].strip
+      @result.score = temp[1..temp.size-2]
+      @result.numeric_score = JSON.parse(@result.score).first.split(" ").first
+      @result.timer = CalculateSeconds.new(@result.numeric_score.split(" ").first).convert
+      @result.gameday_id = Gameday.find_or_create_by!(date: (Date.new(2024, 10, 7) + @result.edition).strftime("%Y-%m-%d")).id
+    rescue
+      # Silently catch any errors
+    end
     @result
   end
 end
--- app/services/parse_zip.rb
+++ app/services/parse_zip.rb
@@ -6,13 +6,17 @@ class ParseZip
   end
 
   def parse
-    temp = @result.original.split("\n")
-    @result.edition = temp[0].match(/#(\d+)/)[1]
-    @result.score = temp[0].match(/\d+:\d+/)[0]
-    @result.numeric_score = temp[0].split("|").last.strip
-    @result.timer = CalculateSeconds.new(@result.numeric_score.split(" ").first).convert
-    @result.secondary_timer = temp[1].scan(/\d+/).first.to_i
-    @result.gameday_id = Gameday.find_or_create_by!(date: (Date.new(2025, 3, 17) + @result.edition).strftime("%Y-%m-%d")).id
+    begin
+      temp = @result.original.split("\n")
+      @result.edition = temp[0].match(/#(\d+)/)[1]
+      @result.score = temp[0].match(/\d+:\d+/)[0]
+      @result.numeric_score = temp[0].split("|").last.strip
+      @result.timer = CalculateSeconds.new(@result.numeric_score.split(" ").first).convert
+      @result.secondary_timer = temp[1].scan(/\d+/).first.to_i
+      @result.gameday_id = Gameday.find_or_create_by!(date: (Date.new(2025, 3, 17) + @result.edition).strftime("%Y-%m-%d")).id
+    rescue
+      # Do nothing, just return @result as-is
+    end
     @result
   end
 end
